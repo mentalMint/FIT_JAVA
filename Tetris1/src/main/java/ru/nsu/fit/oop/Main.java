54ad30a9bd03bd50b91559a3956d586b959919fc
mentalMint <90881799+mentalMint@users.noreply.github.com>
2022-05-21 19:44:29 +0700

    Delete Tetris1 directory

--- Tetris1/src/main/java/ru/nsu/fit/oop/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.nsu.fit.oop;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Controller controller = new Controller();
-        controller.run();
-    }
-}
